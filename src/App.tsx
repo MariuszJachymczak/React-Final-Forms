da50c03beb0d85371715ab91bef13cd4a1615a01
mariusz.jachymczak <mariusz.jachymczak@gazeta.pl>
2023-11-12 19:34:41 +0100

    basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import "./App.css";
+import "./components/styles/FormStyles.css";
 import MyForm from "./components/MyForm";
 import MyMultiSelectForm from "./components/MyMultiSelectForm";
 
 function App() {
   return (
-    <div className="App-header">
-      {/* <MyForm /> */}
-      <MyMultiSelectForm />
+    <div className="main-container">
+      <div className="my-form">
+        <MyForm />
+      </div>
+      <div className="my-multiselect">
+        <MyMultiSelectForm />
+      </div>
     </div>
   );
 }
